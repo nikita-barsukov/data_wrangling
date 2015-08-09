f12a836766af028ac4cb77e55570a7bad5e1befa
Nikita Barsukov <nikita@barsukov.net>
2015-08-09 15:19:53 +0200

    updated writeup and code according to feedback

--- python/bad_websites.py
+++ python/bad_websites.py
@@ -5,7 +5,7 @@ client = MongoClient("mongodb://localhost:27017")
 coll_kh = client['osm']['kharkiv']
 
 print("Documents with given website: ", coll_kh.count({'website': {'$exists': True}}))
-websites = list(coll_kh.find({'website': {'$exists': True}}, {'website': 1, 'name': 1, '_id': 0}))
+websites = list(coll_kh.find({'website': {'$exists': True}}, {'website': 1, 'name': 1}))
 
 for site in websites:
     url = site['website']
@@ -18,4 +18,8 @@ for site in websites:
         r = requests.get(url)
     except:
         print(url, ' connection error')
+        rec = coll_kh.find_one({'_id': site['_id']})
+        del(rec['website'])
+        coll_kh.save(rec)
+        print('Website deleted')
         continue
